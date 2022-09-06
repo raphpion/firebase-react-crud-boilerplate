012a22b01006e7341cea82f3955c80488c113e66
Raphaël Pion <raph.pion@gmail.com>
2022-09-06 17:28:57 -0400

    #3 Implement delete user account

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { auth } from './firebaseConfig';
-import { login, logout, register } from './controller/user.controller';
+import { deleteAccount, login, logout, register } from './controller/user.controller';
 
 function App() {
   const [user, setUser] = useState(auth.currentUser);
 
+  const [formAction, setFormAction] = useState('login');
   const [formEmail, setFormEmail] = useState('');
   const [formPassword, setFormPassword] = useState('');
   const [formFirstName, setFormFirstName] = useState('');
@@ -29,22 +30,46 @@ function App() {
     await logout();
   }
 
+  const handleClickDeleteAccount = async () => {
+    // eslint-disable-next-line no-restricted-globals
+    const confirmed = confirm("WARNING: Are you sure you wish to delete your account?\nThis action is irreversible.");
+    if (!confirmed) return;
+    await deleteAccount();
+  }
+
   return <div className="App">
     {user
       ? <>
         <h1>Welcome!</h1>
         <p>{user.email}</p>
         <p><button onClick={handleClickLogout}>Logout</button></p>
+        <p><button onClick={handleClickDeleteAccount}>Delete account</button></p>
       </>
       : <>
-        <h1>Welcome! Please sign in.</h1>
+        <h1>{formAction === 'login' ? 'Login' : 'Register'}</h1>
+        <p><label htmlFor="action">I want to</label><select id="action" value={formAction} onChange={(event) => setFormAction(event.target.value)}>
+          <option value="login">Login to an existing account</option>
+          <option value="register">Create an account</option>
+        </select></p>
         <p><label htmlFor="email">Email</label><input id="email" type="email" onChange={(event) => setFormEmail(event.target.value)} /></p>
         <p>
-          <label htmlFor='password'>Password</label>
+          <label htmlFor="password">Password</label>
           <input id="password" type={showPassword ? "text" : "password"} onChange={(event) => setFormPassword(event.target.value)} />
           <button type="button" onClick={() => setShowPassword(!showPassword)}>{showPassword ? "Hide" : "Show"}</button>
         </p>
-        <p><button onClick={handleClickRegister}>Register</button><button onClick={handleClickLogin}>Login</button></p>
+        {formAction === 'register'
+          ? <>
+            <p>
+              <label htmlFor="first-name">First name</label>
+              <input id="first-name" type="text" onChange={(event) => setFormFirstName(event.target.value)} />
+            </p>
+            <p>
+              <label htmlFor="last-name">Last name</label>
+              <input id="last-name" type="text" onChange={(event) => setFormLastName(event.target.value)} />
+            </p>
+            <p><button disabled={!(formFirstName && formLastName && formEmail && formPassword)} onClick={handleClickRegister}>Register</button></p>
+          </>
+          : <p><button onClick={handleClickLogin}>Login</button></p>}
       </>
     }
   </div>;
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,13 +1,12 @@
 import { auth, db } from '../firebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, deleteUser, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { doc, deleteDoc, setDoc } from 'firebase/firestore';
 import User from '../models/user.model';
 
 export async function register(email: string, password: string, firstName: string, lastName: string): Promise<User> {
   const userCredential = await createUserWithEmailAndPassword(auth, email, password);
   const user = { id: userCredential.user.uid, email, firstName, lastName } as User;
   await setDoc(doc(db, 'users', user.id), user);
-  console.log('Account created successfully!');
   return user;
 }
 
@@ -17,6 +16,12 @@ export async function login(email: string, password: string): Promise<void> {
 }
 
 export async function logout(): Promise<void> {
-  await signOut(auth);
-  console.log('Logout successful!');
+  return signOut(auth);
+}
+
+export async function deleteAccount(): Promise<void> {
+  const currentUser = auth.currentUser;
+  if (!currentUser) throw new Error('There is no currently authenticated User!');
+  await deleteDoc(doc(db, 'users', currentUser.uid));
+  await deleteUser(currentUser);
 }
